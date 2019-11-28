437ee98489107d71772cc67989af54dc7781c034
Harry Tang <harry@powerkernel.com>
2019-11-28 22:23:23 +0200

    add frontend

---
+++ build/precache-manifest.1c60e8a47d2a51bd92088849e9b94ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7416491aec4df28914701f796299f21c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d724fc7e67e0e79b41",
+    "url": "/static/css/main.84074ae6.chunk.css"
+  },
+  {
+    "revision": "617411b7fcc24c3ea315",
+    "url": "/static/js/2.092245ec.chunk.js"
+  },
+  {
+    "revision": "15d724fc7e67e0e79b41",
+    "url": "/static/js/main.e3ff0942.chunk.js"
+  },
+  {
+    "revision": "d1973d96e61dcdb1b4d5",
+    "url": "/static/js/runtime-main.039e5682.js"
+  }
+]);
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(morgan((tokens, req, res)=>{
         JSON.stringify(req.body)
     ].join(' ')
 }));
-
+app.use(express.static('build'));
 
 let persons = [
     {
