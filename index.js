df289c8a1013eda276f4617063288a75d5d2f131
Harry Tang <harry@powerkernel.com>
2019-12-06 00:02:38 +0200

    Exercise 3.18

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
  * @link https://powerkernel.com
  * @copyright Copyright (c) 2019 Power Kernel
  */
-
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
+// importing
+const Person = require('./models/person');
+
+// config
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan((tokens, req, res)=>{
+app.use(morgan((tokens, req, res) => {
     return [
         tokens.method(req, res),
         tokens.url(req, res),
@@ -24,53 +28,37 @@ app.use(morgan((tokens, req, res)=>{
 }));
 app.use(express.static('build'));
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-];
-
+// list all
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find().then(persons => {
+        res.json(persons)
+    });
 });
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(p => p.id === id);
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+// get individual person
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(error => next(error))
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(note => note.id !== id);
-    res.status(204).end()
+// delete a person
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error));
 });
 
-generateID = (length=6)=> {
-    const min = Math.pow(10, length-1);
-    const max = Math.pow(10, length)-1;
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-
+// create person
 app.post('/api/persons', (req, res) => {
-    const id = generateID(9);
     const body = req.body;
 
     if (!body.name && !body.number) {
@@ -79,27 +67,72 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    if(persons.find(person=>person.name.search(new RegExp(body.name, 'i'))>-1)){
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    // name unique
+    Person.findOne({'name': body.name})
+        .then(person => {
+            if (person) {
+                return res.status(400).json({
+                    error: 'name must be unique'
+                })
+            } else {
+                const person = {
+                    name: body.name,
+                    number: body.number,
+                };
+
+                Person.create(person)
+                    .then(person => {
+                        res.json(person)
+                    })
+            }
+        });
+});
+
+// update person
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
 
     const person = {
         name: body.name,
         number: body.number,
-        id: id
     };
 
-    persons = persons.concat(person);
-    res.json(person);
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(updatedNote => {
+            res.json(updatedNote.toJSON())
+        })
+        .catch(error => next(error))
 });
 
+// info
 app.get('/info', (req, res) => {
-    res.send(`phonbook has ${persons.length} people.<br/> ${new Date()}`)
+    Person.count('name')
+        .then(total => {
+            res.send(`phonbook has ${total} people.<br/> ${new Date()}`)
+        });
+
 });
 
-const PORT = process.env.PORT || 3001;
+/* handler of requests with unknown endpoint */
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unknown endpoint'})
+};
+app.use(unknownEndpoint);
+
+/* error handler */
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return response.status(400).send({error: 'ID format invalid'})
+    }
+
+    next(error)
+};
+app.use(errorHandler);
+
+// start server
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
---
+++ models/person.js
@@ -0,0 +1,38 @@
+/**
+ * @author Harry Tang <harry@powerkernel.com>
+ * @link https://powerkernel.com
+ * @copyright Copyright (c) 2019 Power Kernel
+ */
+
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+
+console.log('connecting to', url);
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then(_ => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    });
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v
+    }
+});
+
+module.exports = mongoose.model('Person', personSchema);
